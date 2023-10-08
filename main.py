cbea80096f3f675d88aabfdda6ede8086220489b
Max Aumard <max.aumard@uha.fr>
2023-10-08 20:11:14 +0200

    remove print

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
     ])
 
     # Device setup
-    print(torch.cuda.is_available())
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Device: {}".format(device))
     # Hyperparameters
